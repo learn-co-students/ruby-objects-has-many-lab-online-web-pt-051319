3c4a544941ac8fda5b471023d3aaa111fef6beba
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-12 18:46:11 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,10 @@
-class Post
-  attr_accessor :title, :author
+class Author
+  attr_accessor :name
 
   @@all = []
 
-  def initialize(title)
-    @title = title
+  def initialize(name)
+    @name = name
     @@all << self
   end
 
@@ -12,7 +12,20 @@ class Post
     @@all
   end
 
-  def author_name
-    author.name if author
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def self.post_count
+    Post.all.count
   end
 end
