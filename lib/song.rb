68cb9d993219645aa79d11c843fb89952db72bf1
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-12 18:43:35 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song 
+  attr_accessor :name, :artist
+  @@all = [] 
+  
+  def initialize(name)
+    @@all << self
+    @name = name 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def artist_name
+    if @artist == nil
+      return nil
+    else
+      @artist.name
+    end
+  end
+  
+end 
